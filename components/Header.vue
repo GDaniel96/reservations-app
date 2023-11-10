ddb49eb82ba4c847b6c5171ca1b0a97b8bb40345
GDaniel96 <george.daniel1996@yahoo.com>
2023-11-10 23:10:08 +0200

    implemented unfunctioal calendar that maps through the days of the current month and displays them in the calendar body

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
   <nav
     class="w-full h-13 py-4 flex gap-2 justify-center items-center bg-[#BCA37F] text-white"
   >
+    <i class="fa-solid fa-calendar-days fa-2xl"></i>
     <NuxtLink to="/">
       <button class="p-2 bg-[#113946] rounded">Home</button>
     </NuxtLink>
--- pages/new.vue
+++ pages/new.vue
@@ -1,5 +1,69 @@
-<template>test test</template>
+<template>
+  <div class="calendar-container w-[500px] bg-[#4B4B4F] rounded-xl">
+    <header
+      class="calendar-header flex items-center justify-between pt-6 px-8 pb-2.5"
+    >
+      <p class="calendar-current-date text-2xl">November</p>
+      <div class="calendar-navigation flex">
+        <span
+          id="calendar-prev"
+          class="h-9 w-9 cursor-pointer rounded-full leading-9 text-2xl text-[#aeabab] hover:bg-[#f2f2f2] pl-2"
+          ><i class="fa-solid fa-arrow-left"></i
+        ></span>
+        <span
+          id="calendar-next"
+          class="h-9 w-9 cursor-pointer rounded-full leading-9 text-2xl text-[#aeabab] hover:bg-[#f2f2f2] pl-2"
+          ><i class="fa-solid fa-arrow-right"></i
+        ></span>
+      </div>
+    </header>
+    <div class="calendar-body pt-6 px-8 pb-2.5">
+      <ul class="calendar-days flex align-center text-2xl gap-6">
+        <li>Sun</li>
+        <li>Mon</li>
+        <li>Tue</li>
+        <li>Wed</li>
+        <li>Thu</li>
+        <li>Fri</li>
+        <li>Sat</li>
+      </ul>
+      <ul class="calendar-dates flex flex-wrap relative">
+        <li
+          class="text-xl w-[calc(100%/7)] text-center"
+          v-for="day in numberOfDays"
+        >
+          {{ day }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const date = new Date();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const numberOfDays = new Date(year, month, 0).getDate();
+
+    return { year, month, numberOfDays };
+  },
+  computed: {},
+};
 </script>
